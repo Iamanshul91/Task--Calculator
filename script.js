0964aff69dda7499d355696f2ee4c0bed40ba4f8
Iamanshul91 <90099394+Iamanshul91@users.noreply.github.com>
2024-01-09 22:21:34 +0530

    added css and js file

---
+++ script.js
@@ -0,0 +1,26 @@
+let input = document.querySelector('input');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            string = "";
+            input.value = string
+        }
+        else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length-1);
+            input.value = string
+        }
+        else {
+            string = string + e.target.innerHTML;
+            input.value = string
+
+        }
+    })
+})
